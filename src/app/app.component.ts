97e72e0c926252c106406bd00b99ce492e51f8fa
minami-choi <cmina2846@gmail.com>
2018-06-01 00:48:22 +0900

    마무리

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,14 @@ export class AppComponent {
 
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link ${link.value}`);
+    this.articles.push(new Article(title.value, link.value));
+    title.value = '';
+    link.value = '';
     return false;
+  }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
   }
 
 }
